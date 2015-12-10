e89cce3779b99c42174d4db30b04557777345888
tony <lostyu789@163.com>
2015-12-10 23:24:10 +0800

    创青春第一版整合，首页，个人，公司表单页

---
+++ cqc/js/common.js
@@ -0,0 +1,138 @@
+/**
+ * Created by colin on 2015/10/12.
+ */
+
+(function(){
+
+    $(".t-form-input").focus(function(){
+        $(this).parent().css("borderColor","#349aff");
+    }).blur(function(){
+        $(this).parent().css("borderColor","#d0d0d0");
+    });
+
+    $(".t-textarea textarea").focus(function(){
+        $(this).parent().css("borderColor","#349aff");
+    }).blur(function(){
+        $(this).parent().css("borderColor","#d0d0d0");
+    });
+
+    $(".d-input,.d-textarea").focus(function(){
+        $(this).css("borderColor","#349aff");
+    }).blur(function(){
+        $(this).css("borderColor","#d0d0d0");
+    });
+
+
+    //
+
+
+
+    $(".y-rg-textarea").focus(function(){
+        $(this).css("borderColor","#349aff");
+    }).blur(function(){
+        $(this).css("borderColor","#d0d0d0");
+    });
+
+    //返回顶部
+    $("#backTop").on("click",move);
+
+    var _top_btn = $(".toolbar");
+
+    $(window).on("scroll",function(){
+        check(300);
+    });
+    function move(){
+        $("html,body").stop().animate({
+            scrollTop:0
+        },300);
+    };
+    function check(pos){
+        if($(window).scrollTop() > pos){
+            $("#backTop").fadeIn();
+        }
+        else{
+            $("#backTop").fadeOut();
+        }
+    };
+    //返回顶部判断
+    $(window).on("scroll",function(){
+        var htop = $(window).scrollTop();
+        var ph = $(window).height();
+        var dh = document.body.scrollHeight;
+        var scroll_top = dh - htop - ph;
+        if (scroll_top <= 261) {
+            _top_btn.css("bottom", 315 - scroll_top);
+        } else {
+            _top_btn.css("bottom", 26);
+        }
+    });
+
+    //搜索框
+    $(".serarch_input").focus(function(){
+        $(this).siblings(".sign").hide();
+        $(this).parent(".serarch").css("borderColor","#349aff");
+        if ($(this).val() == this.defaultValue) {
+            $(this).val("");
+        }
+    }).blur(function(){
+        $(this).siblings(".sign").show();
+        $(this).parent(".serarch").css("borderColor","#dadada")
+        if ($(this).val() == '') {
+            $(this).val(this.defaultValue);
+        }
+    });
+
+})();
+
+
+/**
+ * 验证表单
+ */
+
+//是否为中文
+function isCn(cn){
+    var chinese = /^[\u4E00-\u9FFF]{2,4}$/;
+    return(chinese.test(cn));
+}
+
+//验证手机号码
+function isMobile(str) {
+    if (str.toString().length != 11)
+        return false;
+    var prefix = [130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 147, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189];
+    var re = new RegExp("^(" + prefix.join("|") + ")\\d+$");
+    return re.test(str);
+}
+
+//验证数字
+function isNum1(obj){
+    //非负整数、小数
+    var isNum = /^([1-9]\d*|0)(\.\d{2})?$/;
+    return(isNum.test(obj));
+}
+
+function isNum2(obj){
+    //正浮点数
+    var isNum = /^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([1-9]*[1-9][0-9]*))$/;
+    return(isNum.test(obj));
+}
+
+//是否为身份证
+function isSf(sf){
+    var isIDCard1=/^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$/;//(15位)
+    var isIDCard2=/^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X)$/;//(18位)
+    return(isIDCard1.test(sf) || isIDCard2.test(sf));
+}
+
+//是否为数字
+function isSz(obj){
+    var sz = /^[0-9]*$/;
+    return(sz.test(obj));
+}
+
+//验证邮箱
+function isYx(yx){
+    var reyx = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+    /*var reyx = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;*/
+    return(reyx.test(yx));
+}
--- cqc/js/main.js
+++ cqc/js/main.js
@@ -2,11 +2,67 @@ $(function() {
 	
 });
 
-// 命名空间 众创
-var zc = {};
+var cqc = {};
 
-// ui组件
-zc.ui = {};
+cqc.ui = {};
+cqc.ui.tabs = function(objTabNav, objTabPanel, type) {
+	type = type || 'click';
+	objTabNav.on(type, function() {
+		var index = $(this).index();
 
-// app
-zc.app = {};
+		objTabNav.removeClass('active');
+		$(this).addClass('active');
+		objTabPanel.hide().eq(index).show();
+	});
+};
+cqc.ui.scroll = function(obj) {
+	obj[0].innerHTML += obj[0].innerHTML;
+	var child = obj.children(),
+		len = child.length,
+		h = child.height(),
+		timer = null,
+		iNow = 0;
+
+
+	timer = setInterval(function() {		
+		iNow++;
+		obj.animate({top: -h*iNow+'px'}, function() {
+			if(iNow == len-1){
+				iNow = 0;
+				obj.css('top', 0);
+			}
+		});
+	}, 10000);
+};
+
+cqc.app = {};
+cqc.app.index = function() {
+
+	(function() {
+		$(".d-fw-l").on("mouseenter", function() {
+			$(".d-fw-tc1").fadeIn();
+		});
+		$(".d-fw-r").on("mouseenter", function() {
+			$(".d-fw-tc2").fadeIn();
+		});
+		$(".d-fw-tc").on("mouseleave", function() {
+			$(this).fadeOut();
+		});
+	})();
+	
+	(function() {
+		var $tabsnav1 = $('#tabs-nav1 > .item');
+		var $tabspanel1 = $('#tabs-panel1 > .tab-panel');
+		var $tabsnav2 = $('#tabs-nav2 > h2');
+		var $tabspanel2 = $('#tabs-panel2 > .item');
+
+		cqc.ui.tabs($tabsnav1, $tabspanel1, 'mouseover');
+		cqc.ui.tabs($tabsnav2, $tabspanel2, 'mouseover');
+	})();
+
+	(function() {
+		var $scroll1 = $('#scroll1');
+		cqc.ui.scroll($scroll1);
+	})();
+};
+
