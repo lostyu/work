01330f1f882876c38f94b2bbd32c59a9e37da367
Tony <lostyu789@163.com>
2016-02-19 10:28:53 +0800

    事故快处，个人中心，细节修改

--- jxj/src/js/main.js
+++ jxj/src/js/main.js
@@ -1,5 +1,11 @@
 $(function(){
     main.ui.toggleFt();
+
+
+    var aStarBox = $('.j-m-star');
+    for(var i=0;i<aStarBox.length;i++){
+        main.ui.star(aStarBox.eq(i), 5);
+    }
 });
 
 
@@ -16,5 +22,34 @@ main.ui.toggleFt = function() {
     $toggle.click(function() {
         $(this).toggleClass('active');
         $inner.toggleClass('active');
+    });
+};
+
+/**
+ * 星级评分
+ * @param objStar   父容器
+ * @param defNum    默认星级
+ */
+main.ui.star = function(objStar, defNum) {
+    var aStar = objStar.find('a');
+
+    aStar.each(function(index, elem) {
+        elem.index = index;
+        if(index < defNum){
+            $(elem).addClass('active');
+        }
+    });
+
+    aStar.on('click', function() {
+        var _index = $(this).get(0).index;
+
+        aStar.each(function(index, elem) {
+            $(elem).removeClass('active');
+        });
+        aStar.each(function(index, elem) {
+            if(index <= _index){
+                $(elem).addClass('active');
+            }
+        });
     });
 };
